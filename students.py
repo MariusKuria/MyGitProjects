654222d1bba4e47a71becc1ed26049f1de786999
MariusKuria <marius.dadurka@gmail.com>
2023-11-12 16:04:08 +0200

    upd

--- students.py
+++ students.py
@@ -1,4 +1,17 @@
-students = ["Rimas", "Ona", "Izavele", "Fejona"]
+#students = ["Rimas", "Ona", "Izavele", "Fejona"]
 
-for student in range(len(students)):
-    print(student + 1, students[student]) 
+#for student in range(len(students)):
+#    print(student + 1, students[student]) 
+
+houses = ["Panevezis", "Siauliai", "Ryga"]
+
+students = {
+    "Rimas": "Siauliai",
+    "Ina": "Panevezys",
+    "Fejona": "Kupiskis"
+}
+
+
+
+for student in students:
+    print(student, students[student], sep=", ")
