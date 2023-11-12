90bd3c3a697106e412128393c5df87047a0b8969
MariusKuria <marius.dadurka@gmail.com>
2023-11-12 11:04:27 +0200

    upd

--- cat.py
+++ cat.py
@@ -9,5 +9,20 @@ while i < 3:
 
 # naudojame for
 
-for i in [0, 1, 2, 3, 4]:
+for i in range(3):
     print("meow")
+
+# jei norime kelis kartus isprint ta pati tiesiog zodi padauhina, end 
+# nepalieka tucios eilutes *
+
+print("Kietai\n" * 10, end="")
+
+# kitas cariantas\
+
+while True:
+    n = int(input("Whats n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("kiss")
