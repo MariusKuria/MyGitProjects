edbf9400ea5cbcc6f10c7e53720ade460007950b
MariusKuria <marius.dadurka@gmail.com>
2023-11-16 22:13:00 +0200

    upd

--- error_handling.py
+++ error_handling.py
@@ -1,10 +1,16 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-    
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
-print(f"X is {x}")
-    
+def main():
+    x = get_int()
+    print(f"X is {x}")
+
+def get_int():  
+    while True:
+        try:
+            x = int(input("What's x? "))
+        
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
+main() 
