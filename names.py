e6dadb775cf3a537cead7de140734571bcaf8fb1
MariusKuria <marius.dadurka@gmail.com>
2023-12-03 14:38:46 +0200

    upd

--- names.py
+++ names.py
@@ -1,5 +1,9 @@
-name = input("Please write the name: ")
+for _ in range(3):
+    name = input("Please write the name: ")
 
-file = open("name.txt", "a")
-file.write(name)
-file.close()
+    file = open("name.txt", "a")
+    file.write(f"{name}\n")
+    file.close()
+
+
+4500*70
